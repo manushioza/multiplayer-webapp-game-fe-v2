b6081bd84c7bd6e8588949b5dac7af81d6fc1a52
ishacheema <60581256+ishacheema@users.noreply.github.com>
2023-02-16 23:57:49 -0500

    Adds route to register

--- mutiplayer-webapp-game-fe-v2/src/App.js
+++ mutiplayer-webapp-game-fe-v2/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import {BrowserRouter, Routes, Route,} from "react-router-dom";
 import "./App.css";
 import Login from "./Pages/Login"
+import Register from "./Pages/Register"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
     </Routes>
   </BrowserRouter>
   );
