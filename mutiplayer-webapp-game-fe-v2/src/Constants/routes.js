eae0673f3817199a4e3220f3da5cd6c4db9588be
ishacheema <60581256+ishacheema@users.noreply.github.com>
2023-03-12 22:26:43 -0400

    Adds routes constants

--- mutiplayer-webapp-game-fe-v2/src/Constants/routes.js
+++ mutiplayer-webapp-game-fe-v2/src/Constants/routes.js
@@ -1,2 +1,5 @@
 export const REGISTER_ROUTE = '/register';
 export const HOME_ROUTE = '/home';
+export const MAINSTORY1_ROUTE = '/mainstory1';
+export const MAINSTORY2_ROUTE = '/mainstory2';
+export const CREATEGAME_ROUTE = '/creategame';
